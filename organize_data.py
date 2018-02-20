e88d64615dd3157542997eabd62c9552419f74b6
Presto412 <priyanshjain412@gmail.com>
2018-02-20 12:55:42 +0530

    Initial Commit

---
+++ organize_data.py
@@ -0,0 +1,43 @@
+import json
+from pprint import pprint
+
+with open("usa_heartrate_by_county.csv", "r") as f:
+    lines = [i.strip().split(',') for i in f.readlines()]
+with open("usa_population_area_by_county.csv", "r") as f:
+    census_lines = [i.strip().split(',') for i in f.readlines()]
+with open("usa_bordering_states.csv", "r") as f:
+    state_lines = [i.strip().split(',') for i in f.readlines()]
+
+for county1 in census_lines:
+    for county2 in lines:
+        if (county2[1] in county1[6]) and (county2[0] in county1[5]):
+            county2[4] = county1[9].strip()
+            county2[5] = county1[7].strip()
+            county2[6] = county1[12].strip()
+
+states = list(set([i[0] for i in lines]))
+
+bordering_states = {state2: [i.strip() for i in state1[1:-1]]
+                    for state2 in states for state1 in state_lines if state1[0] == state2}
+
+# for state1 in state_lines:
+#     for state2 in states:
+#         if state1[0] == state2:
+#             bordering_states.update({state2: [i.strip() for i in state1[1:-1]]})
+
+final_dict = {
+    state: {
+        "Counties": [{data[1]: {
+            "heart_disease_DR": data[3],
+            "area": data[4],
+            "population": data[5],
+            "population_density": data[6]
+        }}
+            for data in lines if data[0] == state
+        ],
+        "Bordering States": bordering_states[state] if state in bordering_states else ""
+    }
+    for state in states
+}
+with open("organized_data.json", "w") as f:
+    json.dump(final_dict, f)
