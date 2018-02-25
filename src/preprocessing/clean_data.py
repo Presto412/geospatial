f50ffab6981839df3bae59ab286bbd07af040c22
Presto412 <priyanshjain412@gmail.com>
2018-02-25 20:28:27 +0530

    refactor

--- src/preprocessing/clean_data.py
+++ src/preprocessing/clean_data.py
@@ -1,18 +1,48 @@
+"""Module to clean data
+"""
 import os
 import json
-with open(os.path.join("output", "organized_data.json"), "r") as f:
-    data = json.load(f)
 
 
-def clean(details):
-    return [
-        county for county in details["Counties"]
-        if not any(
-            elem == "" or elem == "-1.00"
-            for elem in county.items()[0][1].values()
-        )
-    ]
+def data_unclean(county):
+    """Checks for missing and unneccessary values
+
+    Arguments:
+        county {[dictionary]} -- [county details]
+
+    Returns:
+        [boolean] -- [true if data is unclean]
+    """
+    return any(elem == -1.00
+               for elem in county.values())
 
 
-final_dict = {state: clean(details) for state, details in data.items()}
-json.dump(final_dict, open(os.path.join("output", "cleaned_data.json"), "w"))
+def clean(counties):
+    """Returns cleaned county data
+
+    Arguments:
+        details {list} -- [list of counties of a state]
+    """
+
+    return [county for county in counties if not data_unclean(county)]
+
+
+def main():
+    """Opens and writes cleaned data to json
+    """
+
+    with open(os.path.join("output", "organized_data.json"), "r") as fobj:
+        data = json.load(fobj)
+    final_dict = {
+        state: {
+            "Counties": clean(details["Counties"]),
+            "Bordering States": details["Bordering States"]
+        }
+        for state, details in data.items()
+    }
+    json.dump(final_dict, open(
+        os.path.join("output", "cleaned_data.json"), "w"))
+
+
+if __name__ == "__main__":
+    main()
--- src/preprocessing/organize_data.py
+++ src/preprocessing/organize_data.py
@@ -1,47 +1,119 @@
+"""Reads and saves csv data to json in organized format
+"""
 import json
 import os
+from decimal import Decimal
 
-with open(os.path.join("data", "usa_heartrate_by_county.csv"), "r") as f:
-    lines = [i.strip().split(',') for i in f.readlines()]
-with open(os.path.join("data", "usa_population_area_by_county.csv"), "r") as f:
-    census_lines = [i.strip().split(',') for i in f.readlines()]
-with open(os.path.join("data", "usa_bordering_states.csv"), "r") as f:
-    state_lines = [i.strip().split(',') for i in f.readlines()]
 
-for county1 in census_lines:
-    for county2 in lines:
-        if (county2[1].upper() in county1[6].upper()) and (county2[0].upper() in county1[5].upper()):
-            county2[4] = county1[9].strip()
-            county2[5] = county1[7].strip()
-            county2[6] = county1[12].strip()
+def get_disease_data(details):
+    """returns only necessary disease data
 
-states = list(set([i[0] for i in lines]))
-
-bordering_states = {
-    state2: [
-        i.strip() for i in state1[1:-1]
-    ]
-    for state2 in states
-    for state1 in state_lines
-    if state1[0] == state2
-}
-
-final_dict = {
-    state: {
-        "Counties": [{
-            data[1]: {
-                "heart_disease_DR": data[3],
-                "area": data[4],
-                "population": data[5],
-                "population_density": data[6]
-            }
-        }
-            for data in lines
-            if data[0] == state
-        ],
-        "Bordering States": bordering_states[state] if state in bordering_states else ""
+    Arguments:
+        details {list} -- details of each county
+    """
+    return {
+        "state_name": details[0].upper(),
+        "name": details[1].upper(),
+        "death_rates": float(Decimal(details[3]))
     }
-    for state in states
-}
-with open(os.path.join("output", "organized_data.json"), "w") as f:
-    json.dump(final_dict, f)
+
+
+def get_census_data(details):
+    """returns only required data from census
+
+    Arguments:
+        details {list} -- details of each county
+    """
+    try:
+        return {
+            "state_name": details[5].split('-')[1].strip().upper(),
+            "name": details[6].upper().replace(' COUNTY', ''),
+            "population": int(details[7].split('(')[0]),
+            "area": float(Decimal(details[9].split('(')[0])),
+        }
+    except IndexError:
+        return None
+
+
+def get_state_data(details):
+    """returns bordering states for a state
+
+    Arguments:
+        details {list} -- details of each state
+    """
+    return {"name": details[0].upper(), "bordering_states": details[1:-1]}
+
+
+def aggregate_county_data(county, census_data):
+    """aggregates county data for given county
+
+    Arguments:
+        county {string}
+        census_data {list}
+    """
+    for item in census_data:
+        if county["name"] in item["name"] and item["state_name"] == county["state_name"]:
+            county.update(item)
+    return county
+
+
+def get_county_data_by_state(state, disease_data, census_data):
+    """gets county data for given state
+
+    Arguments:
+        state {[type]} -- [description]
+        disease_data {[type]} -- [description]
+        census_data {[type]} -- [description]
+    """
+    return [
+        aggregate_county_data(county, census_data) for county in disease_data
+        if county["state_name"] == state["name"]
+    ]
+
+
+def aggregate(disease_data, census_data, state_data):
+    """aggregates all county data
+
+    Arguments:
+        disease_data {list}
+        census_data {list}
+        state_data {list}
+    """
+    return {
+        state["name"]: {
+            "Counties":
+            get_county_data_by_state(state, disease_data, census_data),
+            "Bordering States":
+            state["bordering_states"]
+        }
+        for state in state_data
+    }
+
+
+def main():
+    """Organizes data into /outputs
+    """
+    disease_data_fp = os.path.join("data", "usa_heartrate_by_county.csv")
+    census_data_fp = os.path.join("data", "usa_population_area_by_county.csv")
+    state_data_fp = os.path.join("data", "usa_bordering_states.csv")
+    output_fp = os.path.join("output", "organized_data.json")
+
+    with open(disease_data_fp, "r") as fobj:
+        disease_data = [
+            get_disease_data(i.strip().split(',')) for i in fobj.readlines()
+        ]
+    with open(census_data_fp, "r") as fobj:
+        census_data = [
+            get_census_data(i.strip().split(',')) for i in fobj.readlines()
+            if get_census_data(i.strip().split(',')) is not None
+        ]
+    with open(state_data_fp) as fobj:
+        state_data = [
+            get_state_data(i.strip().split(',')) for i in fobj.readlines()
+        ]
+    json.dump(
+        aggregate(disease_data, census_data, state_data), open(output_fp, "w"))
+
+
+if __name__ == '__main__':
+    main()
